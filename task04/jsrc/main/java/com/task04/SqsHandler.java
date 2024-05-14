57ab1aa0f502654c8e5123ae337450ab3812b42c
Dmytro Osadchyi <tselina@gmail.com>
2024-05-14 21:07:10 +0200

    Task04 (AWS Lambdas & SQS + SNS Integration): -fix errors

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -4,6 +4,11 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.RegionScope;
+import com.syndicate.deployment.model.ResourceType;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,8 +24,8 @@ import java.util.Map;
 		batchSize = 1
 )
 @DependsOn(
-		name = "cmtr-67d6e834-demo-sqs-queue"
-		resourceType = "sqs_queue"
+		name = "cmtr-67d6e834-demo-sqs-queue",
+		resourceType = ResourceType.SQS_QUEUE
 )
 public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
 
